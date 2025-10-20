f8023f0c1661d9dbad0bfcf4151f918dbf5a1da7
dura6399 <dura6399@bears.unco.edu>
2025-10-20 16:23:11 +0000

    test commit 1

---
+++ activity5.py
@@ -0,0 +1,77 @@
+#print(type(words_file))
+#for line in words_file:
+    #print(line)
+#This keeps it from itrating forever. 
+#It will never stop without a defined stopping point
+
+#words = []
+#print(type(words_file))
+#for line in words_file:
+#    print(line)
+    #words.append(line.strip())
+
+
+# print(words)
+#strip gets rid of white space before and after
+
+
+# print([x for x in dir(words_file) if '_' != x[0] ])
+
+# print(words_file.readline())
+# print(words_file.readline())
+
+# data = [1, 3, 2, 8, 5 , 6, 10]
+# print([2*x for x in data if x % 2 == 0])
+
+
+
+# can x be used like a normal variable in commands?
+
+# (route to file, interaction with file)
+# r = read
+# w = overwrites data in file
+# a = appending data to file
+
+#file = open('CROSSWD.txt', 'r')
+
+#def twenty_or_more(file):
+#    file = open(file, 'r')
+#    Ch20 = []
+#    if [x for x in dir(file) if ]
+#        for line in file:
+#            print(line.strip())
+
+
+def more_than_twenty(file):
+    words = []
+    data = open(file, 'r')
+
+#    for word in data:
+#        if len(word.strip()) > 20:
+#            words.append(word.strip())
+
+    words = [x.strip() for x in data if len(x.strip()) > 20]
+    return words
+
+print(more_than_twenty("CROSSWD.txt"))
+
+def has_no_e(word):
+    word = []
+    if 'e' in len(word.strip()): 
+        return True
+    else:
+        return False
+
+
+
+#def uses_only(word, letters)
+    
+#def all_uses_only(file, letters)
+
+
+
+
+
+
+
+
